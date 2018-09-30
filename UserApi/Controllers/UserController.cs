1f937b65bbee9cf03442a558d244c025468aafe2
prabhat pandey <prabhat pandey@LAPTOP-7ACR6C2G>
2018-09-30 19:39:37 +0530

    Creating The repo

---
+++ UserApi/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UserApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        [Route("Users")]
+        public string RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("Login")]
+        public string Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("User")]
+        public string CheckUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("Password")]
+        public string ResetPassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserApi/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UserApi.Controllers
+{
+    public class VendorController : ApiController
+    {
+        public void GetVendor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
